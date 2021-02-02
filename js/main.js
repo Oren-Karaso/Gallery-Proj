e4fbf02e53cb5a64dc57b425dd6c756ccc8fecce
Oren Karaso <orenkaraso@gmail.com>
2021-02-02 21:30:57 +0200

    link again

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function renderProj() {
                   <li>Date: ${currProj.publishedAt}</li>
                   <li>${currProj.name}</li>
                   <li>Category: ${currProj.labels[1]}</li>
-                  <li>Play the game!: ${currProj.url}</li>
+                  <li> ${currProj.url}</li>
                 </ul>
                 <button class="btn btn-primary" data-dismiss="modal" type="button">
                   <i class="fa fa-times"></i>
